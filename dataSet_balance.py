ba19652f6f9d738f7768d8bcd781f573b7d71cfd
faker1897 <yangyunsong040529@163.com>
2025-05-20 19:52:40 -0700

    commit first try

---
+++ dataSet_balance.py
@@ -0,0 +1,16 @@
+import os
+import tensorflow as tf
+directory = "dataSet/train"
+
+#
+class_counts = {}
+for class_name in os.listdir(directory):
+    class_path = os.path.join(directory, class_name)
+    if os.path.isdir(class_path):
+        num_images = len(os.listdir(class_path))
+        class_counts[class_name] = num_images
+
+print(class_counts)
+
+print(tf.config.list_physical_devices('GPU'))
+print(tf.__version__)
